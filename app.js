0b8f21ddd4e6795fc4b0cac515cb2ba59023e935
Michelle Stermitz <stermitzm@gmail.com>
2020-07-06 15:54:41 -0700

    added event listener

--- app.js
+++ app.js
@@ -1,11 +1,30 @@
 // import functions and grab DOM elements
-import { countsAsAYes } from ''
 
 const myButton = document.getElementById(button);
-console.log myButton;
+console.log(myButton);
 
 const score = document.getElementById(Result);
-console.log score;
+console.log(score);
 // initialize state
 
 // set event listeners to update state and DOM
+
+myButton.addEventListener('click', () => {
+    const name = prompt("What's your name?");
+    console.log(name);
+
+    const consent = confirm('Do you really want to take this quiz?');
+    if (consent === false) {
+        return;
+    }
+
+    const answer1 = prompt('Are unicorns beautiful?');
+      //Answer: yes
+
+    const answer2 = prompt('Are unicorns the same thing as narwhals?');
+      //Answer: no
+
+    const answer3 = prompt('Are unicorns real?');
+     //Answer: yes
+});
+
